58f4736c8cd273299b4b485cb417015cf67354c2
Laudson Euller <laudsonps2@hotmail.com>
2026-09-21 20:51:38 -0300

    feat: create user entity

---
+++ src/main/java/br/com/laudson/usuario_api/model/Usuario.java
@@ -0,0 +1,20 @@
+package br.com.laudson.usuario_api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+}
