9897ab2e91a503fdbe47b6e6887915c66dc6e26d
Laudson Euller <laudsonps2@hotmail.com>
2026-09-21 21:54:42 -0300

    feat: create user repository

---
+++ src/main/java/br/com/laudson/usuario_api/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package br.com.laudson.usuario_api.repository;
+
+import br.com.laudson.usuario_api.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
