a1b050a1ca26002cb81fcb0eb5eda0ae162a08f7
Laudson Euller <laudsonps2@hotmail.com>
2026-09-21 22:15:39 -0300

    feat: create user service

---
+++ src/main/java/br/com/laudson/usuario_api/service/UsuarioService.java
@@ -0,0 +1,14 @@
+package br.com.laudson.usuario_api.service;
+
+import br.com.laudson.usuario_api.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+}
