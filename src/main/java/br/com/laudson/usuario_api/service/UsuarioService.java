0564e91226e9282fc3482a5fe10687effece93b7
Laudson Euller <laudsonps2@hotmail.com>
2026-09-24 18:17:57 -0300

    feat: hash user passwords with bcrypt

--- src/main/java/br/com/laudson/usuario_api/service/UsuarioService.java
+++ src/main/java/br/com/laudson/usuario_api/service/UsuarioService.java
@@ -4,22 +4,28 @@ import br.com.laudson.usuario_api.dto.CriarUsuarioRequestDTO;
 import br.com.laudson.usuario_api.dto.CriarUsuarioResponseDTO;
 import br.com.laudson.usuario_api.model.Usuario;
 import br.com.laudson.usuario_api.repository.UsuarioRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UsuarioService(UsuarioRepository usuarioRepository) {
+    public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
         this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public CriarUsuarioResponseDTO criarUsuario(CriarUsuarioRequestDTO request){
+
+        String senhaHash = passwordEncoder.encode(request.senha());
+
         Usuario usuario = new Usuario(
                 request.nome(),
                 request.email(),
-                request.senha()
+                senhaHash
         );
 
         usuarioRepository.save(usuario);
